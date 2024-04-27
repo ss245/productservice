7dfa1407483292a5ea87f2322ee9748ee0fe7355
Shivkumar Shinde <shindeshivkumar33@gmail.com>
2024-04-27 20:28:32 +0530

    Added repository layer.

---
+++ src/main/java/com/ecommerce/productservice/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.ecommerce.productservice.repository;
+
+import com.ecommerce.productservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    Optional<User> findById(Long aLong);
+    Optional<User> findByName(String name);
+    List<User> findByNameEndingWith(String suffix);
+
+    @Override
+    User save(User entity);
+}
